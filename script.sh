cb2e0b36231f25b355f47160cb79c45e8710a236
StopThatTalace <117742366+StopThatTalace@users.noreply.github.com>
2024-02-04 12:23:22 +0100

    update - typo and message

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DISCORD_WEBHOOK_URL="your_url"
-SPECIFIC_CONTAINER_NAMES=("name_container" "name_container")
+DISCORD_WEBHOOK_URL="your_webhook"
+SPECIFIC_CONTAINER_NAMES=("your_container" "your_container")
 
 # Monitor Docker events
 docker events --format '{{json .}}' | while read -r event; do
@@ -11,16 +11,15 @@ docker events --format '{{json .}}' | while read -r event; do
   if [ "$container_name" ]; then
 
     if [[ "${SPECIFIC_CONTAINER_NAMES[@]}" =~ "$container_name" ]]; then
-      
       days=$(date +"%d/%m")
       hours=$(date +"%H:%M")
-      
+
       if [ "$status" == "start" ]; then
         # Trigger webhook for container start
-        curl -X POST -H "Content-Type: application/json" -d '{"content": "Container '"$days **$hours** at **$container_name**"' started! **[ON]**"}' "$DISCORD_WEBHOOK_URL"
+        curl -X POST -H "Content-Type: application/json" -d "{\"content\": \"✅️ - **[Started]** the **$days** at **$hours** - Container **$container_name**\"}" "$DISCORD_WEBHOOK_URL"
       elif [ "$status" == "die" ]; then
-        # Trigger webhook for container stop
-        curl -X POST -H "Content-Type: application/json" -d '{"content": "Container '"$days **$hours** **$container_name**"' stopped **[OFF]**"}' "$DISCORD_WEBHOOK_URL"
+        # Trigger webhook for container stop with formatted date
+        curl -X POST -H "Content-Type: application/json" -d "{\"content\": \":no_entry: - **[Stopped]** the **$days** at **$hours** - Container **$container_name**\"}" "$DISCORD_WEBHOOK_URL"
       fi
     fi
   fi
