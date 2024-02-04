0028b38e377c9664837df6d6d85933d9b73ca9ac
StopThatTalace <117742366+StopThatTalace@users.noreply.github.com>
2024-02-04 12:22:32 +0100

    add - timestamp to message

--- script.sh
+++ script.sh
@@ -9,15 +9,18 @@ docker events --format '{{json .}}' | while read -r event; do
   status=$(echo "$event" | jq -r .status)
 
   if [ "$container_name" ]; then
-    echo "Container Name: $container_name, Status: $status"
 
     if [[ "${SPECIFIC_CONTAINER_NAMES[@]}" =~ "$container_name" ]]; then
+      
+      days=$(date +"%d/%m")
+      hours=$(date +"%H:%M")
+      
       if [ "$status" == "start" ]; then
         # Trigger webhook for container start
-        curl -X POST -H "Content-Type: application/json" -d '{"content": "Container '"**$container_name**"' started! **[ON]**"}' "$DISCORD_WEBHOOK_URL"
+        curl -X POST -H "Content-Type: application/json" -d '{"content": "Container '"$days **$hours** at **$container_name**"' started! **[ON]**"}' "$DISCORD_WEBHOOK_URL"
       elif [ "$status" == "die" ]; then
         # Trigger webhook for container stop
-        curl -X POST -H "Content-Type: application/json" -d '{"content": "Container '"**$container_name**"' stopped **[OFF]**"}' "$DISCORD_WEBHOOK_URL"
+        curl -X POST -H "Content-Type: application/json" -d '{"content": "Container '"$days **$hours** **$container_name**"' stopped **[OFF]**"}' "$DISCORD_WEBHOOK_URL"
       fi
     fi
   fi
